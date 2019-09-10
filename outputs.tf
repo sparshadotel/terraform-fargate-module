3e22f94119c4cef1062ca00d3f70f81b9000bb4f
Sparsha <sparshadotel@gmail.com>
2019-09-10 15:12:14 +0545

    Added Cluster Name to Output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "fargate_task_definition" {
 output "fargate_container_definition" {
   value = var.fargate_container_definitions
 }
+
+output "fargate_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
